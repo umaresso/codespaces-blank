cc86b9a693c2785bb50ff34be51cb9790f8cfd33
umaresso <umarkhatab467@gmail.com>
2022-10-31 17:27:19 +0500

    fixed navbar

--- pages/components/Navbar/Navbar.jsx
+++ pages/components/Navbar/Navbar.jsx
@@ -1,26 +1,26 @@
-import { Box, HStack, Button, Heading } from '@chakra-ui/react'
-import React, { useRef, useEffect, useState } from 'react'
-import Link from 'next/link'
+import { Box, HStack, Button, Heading } from "@chakra-ui/react";
+import React, { useRef, useEffect, useState } from "react";
+import Link from "next/link";
 import {
   getProviderOrSigner,
   connectWallet,
-} from '../../data/accountsConnection'
-import { BigNumber, Contract, ethers, providers, utils } from 'ethers'
-import Web3Modal from 'web3modal'
-import { connect } from 'formik'
+} from "../../data/accountsConnection";
+import { BigNumber, Contract, ethers, providers, utils } from "ethers";
+import Web3Modal from "web3modal";
+import { connect } from "formik";
 
-let NetworkChain = 'goerli'
+let NetworkChain = "goerli";
 function Navbar() {
-  const [walletAddress, setWalletAddress] = useState(null)
-  const web3ModalRef = useRef()
-  let connectionCheckerId = 0
+  const [walletAddress, setWalletAddress] = useState(null);
+  const web3ModalRef = useRef();
+  let connectionCheckerId = 0;
   // theme
-  let textColor = 'white'
-  let background = 'black'
+  let textColor = "white";
+  let background = "black";
   let theme = {
     textColor,
     background,
-  }
+  };
 
   //  _____
 
@@ -30,58 +30,58 @@ function Navbar() {
     getProviderOrSigner(NetworkChain, web3ModalRef, true).then((signer) => {
       if (signer) {
         signer.getAddress().then((user) => {
-          setWalletAddress(user)
-        })
+          setWalletAddress(user);
+        });
       }
-    })
+    });
   }
   useEffect(() => {
     let connectionCheckerId = setInterval(() => {
       if (!walletAddress) {
-        Connect()
+        Connect();
       } else {
-        clearInterval(connectionCheckerId)
+        clearInterval(connectionCheckerId);
       }
-    }, 3000)
-  }, [])
+    }, 3000);
+  }, []);
 
   function getMinimalAddress(_adr) {
-    let adr = _adr.toString()
-    return adr.slice(0, 6) + '..' + adr.slice(40)
+    let adr = _adr.toString();
+    return adr.slice(0, 6) + ".." + adr.slice(40);
   }
-  console.log(walletAddress)
+  console.log(walletAddress);
   return (
     <HStack
-      position={'fixed'}
-      top={'0'}
+      position={"fixed"}
+      top={"0"}
       paddingLeft={5}
       paddingRight={5}
-      justifyContent={'space-between'}
-      width={'100%'}
-      height={'50px'}
-      boxShadow={'1px 1px 1px 1px white'}
+      justifyContent={"space-between"}
+      width={"100%"}
+      height={"50px"}
+      boxShadow={"1px 1px 1px 1px white"}
       {...theme}
-      zIndex={'10'}
+      zIndex={"10"}
     >
-      <Box width={'10vw'}>
+      <Box width={"10vw"}>
         <Link href="/">
           <Heading
-            _hover={{ cursor: 'pointer' }}
+            _hover={{ cursor: "pointer" }}
             as="h5"
-            fontSize={'18px'}
-            width={'max-content'}
+            fontSize={"18px"}
+            width={"max-content"}
           >
             RentWeb3
           </Heading>
         </Link>
       </Box>
       <HStack
-        display={['none', 'none', 'flex']}
+        display={["none", "none", "none", "flex"]}
         spacing={[5, 10, 12]}
         width="40vw"
-        justifyContent={'space-between'}
-        fontSize={['12px', '12px', '14px']}
-        fontWeight={'semibold'}
+        justifyContent={"space-between"}
+        fontSize={["12px", "12px", "14px"]}
+        fontWeight={"semibold"}
       >
         <Link href="/">Home</Link>
         <Link href="/Create">Create</Link>
@@ -91,20 +91,20 @@ function Navbar() {
       </HStack>
 
       <Button
-        colorScheme={'green'}
+        colorScheme={"green"}
         _hover={{
-          cursor: 'pointer',
+          cursor: "pointer",
         }}
-        variant={walletAddress ? 'solid' : 'outline'}
+        variant={walletAddress ? "solid" : "outline"}
         onClick={Connect}
         as="h5"
-        fontSize={'18px'}
-        width={'max-content'}
+        fontSize={"18px"}
+        width={"max-content"}
       >
-        {walletAddress ? getMinimalAddress(walletAddress) : 'Connect'}
+        {walletAddress ? getMinimalAddress(walletAddress) : "Connect"}
       </Button>
     </HStack>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
