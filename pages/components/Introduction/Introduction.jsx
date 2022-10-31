892606274c3dcf13565bb253c38d214db0e0057f
umaresso <umarkhatab467@gmail.com>
2022-10-31 16:45:51 +0500

    fixed introduction image align

--- pages/components/Introduction/Introduction.jsx
+++ pages/components/Introduction/Introduction.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import Link from "next/link";
 import LinkButton from "../LinkButton/LinkButton";
 import { Grid } from "@chakra-ui/react";
-import { VStack } from "@chakra-ui/react";
+import { VStack, Stack } from "@chakra-ui/react";
 
 function Introduction() {
   // theme
@@ -12,33 +12,23 @@ function Introduction() {
   //  _____
 
   return (
-    <Box
-      backgroundImage={
-        "https://cdn.dribbble.com/users/1859790/screenshots/10750520/media/78cf0dccfeebd50268d23057e7568dac.png?compress=1&resize=1200x900&vertical=top"
-      }
-      backgroundRepeat={"no-repeat"}
-      backgroundSize={"cover"}
-    >
-      <Box
-        width={"100%"}
-        color={textColor}
-        height={"100vh"}
-        paddingTop={["60vh", "40vh", "10vh"]}
-        background={"rgba(0,0,0,0.75)"}
-      >
+    <Box width={"100vw"} color={textColor} height={"100vh"} bg={"black"}>
+      <Stack direction={["column", "column", "column", "row"]}>
         <VStack
           align={"left"}
           padding={20}
-          paddingTop={["20vh", "20vh", "5vh"]}
+          paddingBottom={0}
           spacing={3}
+          width={["100%", "100%", "100%", "60%"]}
+          paddingTop={["40vh", "40vh", "20vh"]}
         >
           <Heading
             fontSize={["2em", "4em", "6em"]}
-            width={["80vw", "70vw", "50vw"]}
+            width={["80vw", "80vw", "80vw", "40vw"]}
           >
             Time to Rent Web3 Assets
           </Heading>
-          <Box width={"40vw"}>
+          <Box width={["80vw", "80vw", "80vw", "40vw"]}>
             <Box fontSize={18} textColor={"whiteAlpha.800"}>
               RentWeb3 is a place where you can quickly start Renting your NFTs
               and Various Dapps.We provide you with a simple series of steps to
@@ -57,7 +47,20 @@ function Introduction() {
             variant={"solid"}
           />
         </VStack>
-      </Box>
+
+        <VStack bg={"black"} justifyContent={"center"} height={"100vh"}>
+          <Img
+            height={"fit-content"}
+            width={"90%"}
+            border={"10px solid black"}
+            borderRadius={"40px"}
+            // borderLeft={"150px solid black"}
+            // borderRight={"150px solid black"}
+
+            src="https://cdn.dribbble.com/users/1859790/screenshots/10750520/media/78cf0dccfeebd50268d23057e7568dac.png?compress=1&resize=1200x900&vertical=top"
+          />
+        </VStack>
+      </Stack>
     </Box>
   );
 }
