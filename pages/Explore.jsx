85ddd0e0915b6a3bc432dce0f481ae40e675771a
umaresso <umarkhatab467@gmail.com>
2022-11-06 14:18:57 +0500

    improved Explore page UI__

--- pages/Explore.jsx
+++ pages/Explore.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import {
   VStack,
   Text,
@@ -7,58 +7,56 @@ import {
   HStack,
   WrapItem,
   Wrap,
-} from '@chakra-ui/react'
-import LinkButton from './components/LinkButton/LinkButton'
-import Head from 'next/head'
+} from "@chakra-ui/react";
+import LinkButton from "./components/LinkButton/LinkButton";
+import Head from "next/head";
 function Explore() {
   return (
-    <VStack  height={'fit-content'} bg="black" textColor={'white'}>
+    <VStack height={"100vh"} bg="black" textColor={"white"}>
       <Center>
-        <VStack paddingTop={"10vh"}>
-          <Heading paddingTop={'10vh'} fontSize={'4.5em'} width={'50vw'}>
-            Your Destination to Rent Web3 Assets
+        <VStack paddingTop={"15vh"} spacing={5}>
+          <Heading fontSize={"4em"} width={"50vw"}>
+            Explore Web3 Assets
           </Heading>
           <Text
-            fontFamily={'sans-serif'}
-            textColor={'grey'}
-            fontSize={'18px'}
-            width={'50vw'}
+            fontFamily={"sans-serif"}
+            textColor={"grey"}
+            fontSize={"18px"}
+            width={"50vw"}
           >
-            We provide you with the oporunity to rent NFTs of famous collections
-            and Dapps ,made by the best developers in the space,for showing your
-            NFT collections in an easy way. So you have an NFT from a collection
-            that is not rentable ? Dont worry then even, we got you covered. Our
-            smooth workflow is designed to make non-compatible things compatible
-            in a no-code way. So what are you waiting For ?
+            Explore NFTs of famous collections and rent Dapps made by the best
+            developers in the space for showing your NFT collections with
+            no-code at affordable prices. 
+          <Heading fontSize={"18px"}>So what are you waiting For ?</Heading>
+            
           </Text>
-          <hr/>
-          <Heading padding={"5vh 0 5vh 0"}>Take Action</Heading>
-<Center>
-<Wrap  paddingBottom={"10vh"} spacing={20}>
-          <WrapItem>
-            <LinkButton
-              title={'Explore NFTs'}
-              href={'/ExploreNfts'}
-              color={'blue'}
-              variant={'solid'}
-            />
-          </WrapItem>
-          <WrapItem>
-            <LinkButton
-              title={'Explore Dapps'}
-              href={'/ExploreDapps'}
-              color={'green'}
-              variant={'solid'}
-            />
-          </WrapItem>
-        </Wrap>
-
-
-</Center>
+          
+          <hr />
+          <Heading padding={"2vh 0 2vh 0"}>Take Action</Heading>
+          <Center>
+            <Wrap paddingBottom={"vh"} spacing={20}>
+              <WrapItem>
+                <LinkButton
+                  title={"Explore NFTs"}
+                  href={"/ExploreNfts"}
+                  color={"blue"}
+                  variant={"solid"}
+                />
+              </WrapItem>
+              <WrapItem>
+                <LinkButton
+                  title={"Explore Dapps"}
+                  href={"/ExploreDapps"}
+                  color={"green"}
+                  variant={"solid"}
+                />
+              </WrapItem>
+            </Wrap>
+          </Center>
         </VStack>
       </Center>
     </VStack>
-  )
+  );
 }
 
-export default Explore
+export default Explore;
