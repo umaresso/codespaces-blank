02b904108246abf330644d85f5886e8fb0e9a90d
umaresso <umarkhatab467@gmail.com>
2022-11-07 16:49:09 +0500

    added coming soon in about page

--- pages/About.jsx
+++ pages/About.jsx
@@ -1,10 +1,19 @@
+import { Center, Heading } from '@chakra-ui/react'
+import Head from 'next/head'
 import React from 'react'
 
 function About() {
+  let textColor="white";
   return (
-    <div>
-      About
-    </div>
+    <Center
+      height={["fit-content", "fit-content", "100vh"]}
+      justify={"center"}
+      bg="black"
+      color={textColor}
+      flexDirection={"column"}
+    >
+      <Heading>Coming Soon...</Heading>
+    </Center>
   )
 }
 
